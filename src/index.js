45eb765eee0bd639d9c6645aba6b39c74c98ae30
Joseph Martin <josephmartin1350@gmail.com>
2021-12-10 11:43:48 -0700

    commit tree 835884d803b62b86e2a1d7080ad8fe89856dd5f9

--- src/index.js
+++ src/index.js
@@ -13,3 +13,101 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+
+  // urls
+allToysURL = 'http://localhost:3000/toys'
+
+  // grab stuff
+const toyContainer = document.querySelector('#toy-collection')
+const newToyForm = document.querySelector('form')
+const input = document.querySelectorAll('.input-text')
+
+  // convert node list
+const inputArray = Array.from(input)
+
+  // helpers
+const create = el => document.createElement(el)
+
+  // event listener
+newToyForm.addEventListener('submit', handleSubmit)
+
+
+  // submit callback
+function handleSubmit(e) {
+  e.preventDefault()
+  let toyObj = {
+      name: e.target.name.value,
+      image: e.target.image.value,
+      likes: 0
+  }
+  renderToyCard(toyObj)
+  postNewToy(toyObj)
+}
+
+
+
+  // renderCard
+function renderToyCard(toy) {
+    // make stuff
+  const card = create('div')
+  const toyName = create('h2')
+  const img = create('img')
+  const likesNum = create('likes')
+  const likesBtn = create('button')
+
+    // assign attributes
+  card.className = 'card'
+  toyName.innerText = toy.name
+  img.className = 'toy-avatar'
+  img.src = toy.image
+  likesNum.innerText = toy.likes + ' likes'
+  likesBtn.className = 'like-btn'
+  likesBtn.id = toy.id
+  likesBtn.innerText = 'Like'
+
+    // addEventListeners
+  likesBtn.addEventListener('click', (toys) => {
+    toy.likes += 1
+    likesNum.textContent = toy.likes + ' likes'
+    updateLikes(toy)
+  })
+
+
+    // append stuff
+  card.append(toyName, img, likesNum, likesBtn)
+  toyContainer.append(card)
+}
+
+
+
+  // CRUD
+  fetch(allToysURL)
+    .then(r => r.json())
+    .then(toysArray => toysArray.forEach(toy => renderToyCard(toy)))
+
+function postNewToy(toyObj) {
+  fetch(allToysURL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify(toyObj)
+  })
+  .then(r => r.json())
+  .then(toy => console.log(toy))
+}
+
+function updateLikes(toy) {
+  fetch(`${allToysURL}/${toy.id}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(toy)
+  })
+  .then(r => r.json())
+  .then(toy => console.log(toy))
+}
